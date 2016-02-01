762e9b5dee440f26adf9f27f75a00d739af620e6
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-01-31 23:33:40 -0500

    orbs
    fun with orbs

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup () {
-  createCanvas(600,600);
+ 
 }
 
 function draw() {
-
+createCanvas(600,600);
+if (mouseY <=300) {
+  background(200,100,70)}
+  else {(background(170,50, 20));
 }
+for (var x = 0; x <= width; x += 200) {
+    new orb (x, mouseY, 20);
+    }
+  }
+
+var orb = function(x,y, ellipseSize) {
+    var blues = color(random(0,100), random(100,255), random(100,255));
+  fill(blues);
+ 
+  ellipse(x, mouseY, mouseX-200, mouseY-200);
+};
